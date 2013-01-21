a863c7a602f95910389ccb34a627482e9a3c78e4
Tang Weiqiang <weiqiang.tang@ericsson.com>
2013-01-21 16:08:05 +0800

    add new project for system monitor feature

---
+++ monitor.timeline/src/main/java/igeektang/com/monitor/timeline/App.java
@@ -0,0 +1,13 @@
+package igeektang.com.monitor.timeline;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
